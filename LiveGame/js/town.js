3d7f9eda5a1b28597b34c6af73428de87d690d9d
Danielson9712 <55254935+Danielson9712@users.noreply.github.com>
2019-11-25 15:59:22 -0500

    Update town.js

--- LiveGame/js/town.js
+++ LiveGame/js/town.js
@@ -7,7 +7,10 @@ class town extends Phaser.Scene {
         this.load.image('background', 'assets/SiegeOfTroy.png');
         this.load.image('horsey','assets/Horse 2 invisible background.png');
         this.load.spritesheet('main', 'assets/Main.png', { frameWidth: 48, frameHeight: 48});
-        this.load.spritesheet('guard', 'assets/Guard.png', {frameWidth: 48, frameHeight: 48});
+        this.load.spritesheet('guard1', 'assets/Guard.png', {frameWidth: 48, frameHeight: 48});
+        this.load.spritesheet('guard2', 'assets/Guard.png', {frameWidth: 48, frameHeight: 48});
+        this.load.spritesheet('guard3', 'assets/Guard.png', {frameWidth: 48, frameHeight: 48});
+        this.load.spritesheet('guard4', 'assets/Guard.png', {frameWidth: 48, frameHeight: 48});
     }
 
     create () {
@@ -43,7 +46,7 @@ class town extends Phaser.Scene {
         mainCharacter.setOffset(0, 0);
 
         mainCharacter.setDataEnabled();
-        mainCharacter.data.set('hp', 100);
+        mainCharacter.data.set('hp', 50);
 
         this.anims.create({
             key: 'left',
@@ -74,15 +77,16 @@ class town extends Phaser.Scene {
         /*
             Guard characters
         */
-            guard1 = this.physics.add.sprite(500, 450, 'guard');
-            guard2 = this.physics.add.sprite(700, 450, 'guard');
-            guard3 = this.physics.add.sprite(900, 450, 'guard');
-            guard4 = this.physics.add.sprite(1100, 450, 'guard');
+            guard1 = this.physics.add.sprite(500, 450, 'guard1');
+            guard2 = this.physics.add.sprite(700, 450, 'guard2');
+            guard3 = this.physics.add.sprite(900, 450, 'guard3');
+            guard4 = this.physics.add.sprite(1100, 450, 'guard4');
 
             guard1.setCollideWorldBounds(true);
             guard2.setCollideWorldBounds(true);
             guard3.setCollideWorldBounds(true);
             guard4.setCollideWorldBounds(true);
+
             guard1.displayHeight = heightGameII * 0.35;
             guard1.displayWidth = widthGameII * 0.18;
             guard2.displayHeight = heightGameII * 0.35;
@@ -114,27 +118,138 @@ class town extends Phaser.Scene {
 
             this.anims.create({
                 key: 'g1attack',
-                frames: this.anims.generateFrameNumbers('guard', {start: 2, end: 4}),
+                frames: this.anims.generateFrameNumbers('guard1', {start: 2, end: 4}),
                 frameRate: 10,
             });
 
             this.anims.create({
                 key: 'g2attack',
-                frames: this.anims.generateFrameNumbers('guard', {start: 2, end: 4}),
+                frames: this.anims.generateFrameNumbers('guard2', {start: 2, end: 4}),
                 frameRate: 10,
             });
 
             this.anims.create({
                 key: 'g3attack',
-                frames: this.anims.generateFrameNumbers('guard', {start: 2, end: 4}),
+                frames: this.anims.generateFrameNumbers('guard3', {start: 2, end: 4}),
                 frameRate: 10,
             });
 
             this.anims.create({
                 key: 'g4attack',
-                frames: this.anims.generateFrameNumbers('guard', {start: 2, end: 4}),
+                frames: this.anims.generateFrameNumbers('guard4', {start: 2, end: 4}),
                 frameRate: 10,
             });
+
+            function hpDecrease () {
+                if (aKey.isDown){
+                    let newhp = guard1.data.get('hp');
+                    if (newhp <= 0){
+                       guard1.destroy();
+                    
+                    }
+                    else {
+                    newhp -= 2;
+                    guard1.data.set('hp', newhp);
+                    }
+                }
+                else {
+                    let dec = Phaser.Math.Between(1,2);
+                    if (dec === 1){
+                        guard1.x += 75;
+                        guard1.play('g1attack');
+                        mainhp = mainCharacter.data.get('hp');
+                        mainhp -= 1;
+                        mainCharacter.data.set('hp', mainhp);
+                    }
+                    else {
+                        guard1.x -= 75;
+                        guard1.play('g1attack');
+                        mainhp = mainCharacter.data.get('hp');
+                        mainhp -= 1;
+                        mainCharacter.data.set('hp', mainhp);
+                    }
+                }
+            };
+    
+            function hpDecrease2 () {
+                if (aKey.isDown){
+                    let newhp = guard2.data.get('hp');
+                    if (newhp <= 0){
+                        guard2.destroy();
+                        
+                    }
+                    else {
+                    newhp -= 2;
+                    guard2.data.set('hp', newhp);
+                    }
+                }
+                else {
+                    let dec = Phaser.Math.Between(1,2);
+                    if (dec === 1){
+                        guard2.x += 5;
+                        guard2.play('g2attack');
+                    }
+                    else {
+                        guard2.x -= 5;
+                        guard2.play('g2attack');
+                    }
+                }
+            };
+    
+            function hpDecrease3 () {
+                if (aKey.isDown){
+                    let newhp = guard3.data.get('hp');
+                    if (newhp == 0){
+                        guard3.destroy();
+                    }
+                    else {
+                    newhp -= 2;
+                    guard3.data.set('hp', newhp);
+                    }
+                }
+                else {
+                    let dec = Phaser.Math.Between(1,2);
+                    if (dec === 1){
+                        guard3.y += 5;
+                        guard3.play('g3attack');
+                        
+                    }
+                    else {
+                        guard3.y -= 10;
+                        guard3.play('g3attack');
+                    }
+                }
+            };
+
+                function hpDecrease4 () {
+                    if (aKey.isDown){
+                        let newhp = guard4.data.get('hp');
+                        if (newhp == 0){
+                            guard4.destroy();
+                        }
+                        else {
+                        newhp -= 2;
+                        guard4.data.set('hp', newhp);
+                        }
+                    }
+                    else {
+                        let dec = Phaser.Math.Between(1,2);
+                        if (dec === 1){
+                            guard4.y += 5;
+                            guard4.play('g4attack');
+                            mainhp = mainCharacter.data.get('hp');
+                            mainhp -= 2;
+                            mainCharacter.data.set('hp', mainhp);
+                        }
+                        else {
+                            guard4.y -= 10;
+                            guard4.play('g4attack');
+                        }
+                    }
+                };
+            
+
+           
         /* 
             Extras
             Key controls 
@@ -143,6 +258,16 @@ class town extends Phaser.Scene {
         cursors = this.input.keyboard.createCursorKeys();
         aKey = this.input.keyboard.addKey('A');
 
+        this.physics.add.overlap(mainCharacter, guard1, hpDecrease, null, this);
+        this.physics.add.overlap(mainCharacter, guard2, hpDecrease2, null, this);
+        this.physics.add.overlap(mainCharacter, guard3, hpDecrease3, null, this);
+        this.physics.add.overlap(mainCharacter, guard4, hpDecrease4, null, this);
+
+        gameOver = this.add.text(50, 50, "GAME OVER", {fontFamily: 'Arial', fontSize: 100, color: '#EE204D'});
+        gameOver.visible = false;
+
+        this.cameras.main.setBounds(0, 0, 2560, heightGameII);
+        this.cameras.main.startFollow(mainCharacter);
 
     }
 
@@ -152,6 +277,7 @@ class town extends Phaser.Scene {
             this.physics.pause();
             mainCharacter.setTint(0xff0000);
             gameOver.visible = true;
+            
         }
 
         //Controls motion when certain keys are pressed down
